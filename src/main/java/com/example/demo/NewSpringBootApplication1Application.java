2a9ed2807d940ac6eac4873a074d1904fd661c0d
phani <phania0379@gmail.com>
2024-09-08 11:56:25 +0530

    repo

---
+++ src/main/java/com/example/demo/NewSpringBootApplication1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSpringBootApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSpringBootApplication1Application.class, args);
+	}
+
+}
